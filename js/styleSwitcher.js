084c8934c1e97dc12d3d7298d026b18191f5353d
Sharique Nadim <66444722+shariquenadim@users.noreply.github.com>
2024-01-07 01:59:59 +0530

    image dark mode added

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,36 +1,46 @@
 
-const links=document.querySelectorAll(".alternate-style");
-	totalLinks=links.length;
+const links = document.querySelectorAll(".alternate-style");
+totalLinks = links.length;
 
 
 function setActiveStyle(color) {
-	for(let i=0; i<totalLinks; i++){
-		if(color === links[i].getAttribute("title")){
+	for (let i = 0; i < totalLinks; i++) {
+		if (color === links[i].getAttribute("title")) {
 			links[i].removeAttribute("disabled");
 		}
-		else{
-			links[i].setAttribute("disabled","true");
+		else {
+			links[i].setAttribute("disabled", "true");
 		}
 	}
-	
+
 }
 
 // body skin
-	const bodySkin=document.querySelectorAll(".body-skin"),
-			totalBodySkin=bodySkin.length;
-		for(let i=0; i<totalBodySkin; i++){
-			bodySkin[i].addEventListener("change",function(){
-				if(this.value === "dark"){
-					document.body.className="dark";
-				}
-				else{
-					document.body.className="";
-				}
-			})
+const bodySkin = document.querySelectorAll(".body-skin"),
+	totalBodySkin = bodySkin.length;
+for (let i = 0; i < totalBodySkin; i++) {
+	bodySkin[i].addEventListener("change", function () {
+		if (this.value === "dark") {
+			document.body.className = "dark";
+			changeLogoImage("dark");
 		}
+		else {
+			document.body.className = "";
+			changeLogoImage("light");
+		}
+	})
+}
+
+function changeLogoImage(mode) {
+	const logoImage = document.querySelector(".logo img");
+	if (mode === "dark") {
+		logoImage.src = "/img/sigwhite.png";
+	} else {
+		logoImage.src = "/img/sigblack.png";
+	}
+}
 
 
-
-document.querySelector(".toggle-style-switcher").addEventListener("click",() =>{
-	document.querySelector(".style-switcher").classList.toggle("open"); 
+document.querySelector(".toggle-style-switcher").addEventListener("click", () => {
+	document.querySelector(".style-switcher").classList.toggle("open");
 })
